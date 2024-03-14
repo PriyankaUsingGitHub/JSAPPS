b554a24ecbcb5f44f7dbfe4dbf027a78aed10fa2
PriyankaUsingGitHub <149198023+PriyankaUsingGitHub@users.noreply.github.com>
2024-03-14 12:16:30 +0000

    container config

---
+++ .devcontainer/basics/variable.js
@@ -0,0 +1,12 @@
+const accountId = 85878
+let accountEmail = "abs@gmail.com"
+var accountPassword="123"
+accountCity="Gorakhpur"
+
+//accountId=2  //if const is already declared thn we will not redeclared the const
+accountEmail="xnsn@gmail.com"
+accountPassword="23"
+accountCity="KLD"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
